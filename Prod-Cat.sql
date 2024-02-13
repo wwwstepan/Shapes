f87d60aeb4de893c106b87656b0dc74c6820f735
Stepan Purk <63333004+wwwstepan@users.noreply.github.com>
2024-02-13 08:03:17 +0300

    Update Prod-Cat.sql +DISTINCT

--- Prod-Cat.sql
+++ Prod-Cat.sql
@@ -1,4 +1,4 @@
-﻿SELECT Products.Name AS ProductName, Categories.Name AS CategoryName
+﻿SELECT DISTINCT Products.Name AS ProductName, Categories.Name AS CategoryName
 FROM CategoriesOfProducts
 LEFT JOIN Products ON CategoriesOfProducts.ProductId = Products.Id
 LEFT JOIN Categories ON CategoriesOfProducts.CategoryId = Categories.Id
