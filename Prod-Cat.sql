ea6a23bc0bfb72912409d8ad75308788684743f4
Stepan Purk <63333004+wwwstepan@users.noreply.github.com>
2024-02-13 07:59:10 +0300

    add sql script - answer2

---
+++ Prod-Cat.sql
@@ -0,0 +1,5 @@
+﻿SELECT Products.Name AS ProductName, Categories.Name AS CategoryName
+FROM CategoriesOfProducts
+LEFT JOIN Products ON CategoriesOfProducts.ProductId = Products.Id
+LEFT JOIN Categories ON CategoriesOfProducts.CategoryId = Categories.Id
+
